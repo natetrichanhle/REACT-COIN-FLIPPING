4357d5796abd5f71fbb0eeca244b26e8c4d8e7b9
Nate Trichanh <natetrichanh@gmail.com>
2022-01-16 13:38:28 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function tossCoin() {
+    return Math.random() > 0.5 ? "heads" : "tails";
+}
+
+function fiveHeads() {
+    return new Promise((resolve, reject) => {
+        // your code here!
+        let headsCount = 0;
+        let attempts = 0;
+        while (headsCount < 5) {
+            attempts++;
+            let result = tossCoin();
+            // console.log(`${result} was flipped`);
+            if (result === "heads") {
+                headsCount++;
+            } else {
+                headsCount = 0;
+            }
+        }
+        if (attempts > 100) {
+            reject(console.log('Flipped more than 100 times? NAH.'))
+        } else {
+            resolve(`It took ${attempts} tries to flip five "heads"`);
+        }
+    });
+}
+fiveHeads()
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+console.log("When does this run now?");
+// runs before it resolves and runs after it rejects
